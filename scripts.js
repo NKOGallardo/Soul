75c7a35528e7486122a0f2552815f1c1678c6f30
NKO_Gallardo <nkogalado@gmail.com>
2025-10-16 19:34:01 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('social');
+
+button.addEventListener('click', () => {
+  window.location.href = 'My-work.html';
+});
+
+function Home() {
+    window.location.href = "My-work.html";
+}
+
+function social() {
+    window.location.href = "socials.html";
+}
