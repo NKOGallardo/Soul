b7ad54ae445f83cd7c6d48a0fb6d4f3778a67d96
NKO_Gallardo <nkogalado@gmail.com>
2025-10-25 21:13:32 +0200

    Implement item click handling and active class management
    Added functionality to handle item click events and manage active classes.

--- scripts.js
+++ scripts.js
@@ -11,3 +11,18 @@ function Home() {
 function social() {
     window.location.href = "socials.html";
 }
+
+const ITEMS = document.querySelectorAll('.item')
+ITEMS.forEach(item =>{
+  item.addEventListener('click',() => {
+      removeActionClasses()
+      item.classList.add('active')
+  })
+})
+function removeActionClasses(){
+    ITEMS.forEach(item =>{
+        item.classList.remove('active')
+})}
+
+let currentIndex = 0; 
+const items = document.querySelectorAll('.item');
